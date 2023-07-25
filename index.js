179c45d7d0779bc8ab608c5f449b1e4703b86c97
Tuhfatu-Akhtary <tuhfatu98@gmail.com>
2023-07-25 12:57:35 +0600

    Backend

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import {fileURLToPath} from "url";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { register } from "./controllers/auth.js";
+import {createPost} from "./controllers/posts.js";
+import {verifyToken} from "./middleware/auth.js";
 
 /* Configurations */
 
@@ -38,6 +44,14 @@ const storage=multer.diskStorage({
 
 const upload=multer({storage});
 
+/* Routes with Files*/
+app.post("/auth/register",upload.single("picture"),register);
+app.post("/posts",verifyToken,upload.single("picture"),createPost);
+/* Routes */
+
+app.use("/auth",authRoutes);
+app.use("/users",userRoutes);
+app.use("/posts",postRoutes);
 /* Monggose Setup */
 const PORT=process.env.PORT||6001;
 mongoose.connect(process.env.MONGO_URL,{
